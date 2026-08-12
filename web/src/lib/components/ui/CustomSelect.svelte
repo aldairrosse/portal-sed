dc4b8d5a765b1d2a352fe6761a7d75dacc23000f
Frankil Aldair <fperez@mobo.com.mx>
2026-08-12 16:51:44 -0600

    Remove bottom padding from search input in CustomSelect

--- web/src/lib/components/ui/CustomSelect.svelte
+++ web/src/lib/components/ui/CustomSelect.svelte
@@ -145,7 +145,7 @@
 	ontoggle={handleToggle}
 >
 	{#if searchable}
-		<li class="mb-1 sticky top-0 z-10 bg-base-100 border-b border-base-300 pb-1.5">
+		<li class="mb-1 sticky top-0 z-10 bg-base-100 border-base-300">
 			<input
 				type="search"
 				class="input input-bordered input-xs w-full bg-base-100"
